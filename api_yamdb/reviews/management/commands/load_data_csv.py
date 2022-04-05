333179a077b049d413a5ea298d1503af344772b2
Игорь Живов <izhivov97@yandex.ru>
2022-04-05 18:55:33 +0300

    Исправление отступов по pep8

--- api_yamdb/reviews/management/commands/load_data_csv.py
+++ api_yamdb/reviews/management/commands/load_data_csv.py
@@ -16,35 +16,36 @@ class Command(BaseCommand):
             dict_reader = csv.DictReader(csvfile)
             for row in dict_reader:
                 User.objects.get_or_create(
-                            id=row['id'],
-                            username=row['username'],
-                            email=row['email'],
-                            role=row['role'],
-                            bio=row['bio'],
-                            first_name=row['first_name'],
-                            last_name=row['last_name'])
+                    id=row['id'],
+                    username=row['username'],
+                    email=row['email'],
+                    role=row['role'],
+                    bio=row['bio'],
+                    first_name=row['first_name'],
+                    last_name=row['last_name'])
 
-        with open('static/data/category.csv', 'r', encoding='utf-8') as csvfile:
+        with open('static/data/category.csv', 'r',
+                  encoding='utf-8') as csvfile:
             dict_reader = csv.DictReader(csvfile)
             for row in dict_reader:
                 Category.objects.get_or_create(
-                                    name=row['name'],
-                                    slug=row['slug'])
+                    name=row['name'],
+                    slug=row['slug'])
 
         with open('static/data/genre.csv', 'r', encoding='utf-8') as csvfile:
             dict_reader = csv.DictReader(csvfile)
             for row in dict_reader:
                 Genre.objects.get_or_create(
-                                name=row['name'],
-                                slug=row['slug'])
+                    name=row['name'],
+                    slug=row['slug'])
 
         with open('static/data/titles.csv', 'r', encoding='utf-8') as csvfile:
             dict_reader = csv.DictReader(csvfile)
             for row in dict_reader:
                 Title.objects.get_or_create(
-                                name=row['name'],
-                                year=row['year'],
-                                category_id=row['category'])
+                    name=row['name'],
+                    year=row['year'],
+                    category_id=row['category'])
 
         with open('static/data/genre_title.csv') as csvfile:
             dict_reader = csv.DictReader(csvfile)
@@ -56,19 +57,20 @@ class Command(BaseCommand):
             dict_reader = csv.DictReader(csvfile)
             for row in dict_reader:
                 Review.objects.get_or_create(
-                                title_id=row['title_id'],
-                                text=row['text'],
-                                author_id=row['author'],
-                                score=row['score'],
-                                pub_date=row['pub_date'])
+                    title_id=row['title_id'],
+                    text=row['text'],
+                    author_id=row['author'],
+                    score=row['score'],
+                    pub_date=row['pub_date'])
 
-        with open('static/data/comments.csv', 'r', encoding='utf-8') as csvfile:
+        with open('static/data/comments.csv', 'r',
+                  encoding='utf-8') as csvfile:
             dict_reader = csv.DictReader(csvfile)
             for row in dict_reader:
                 Comment.objects.get_or_create(
-                                review_id=row['review_id'],
-                                text=row['text'],
-                                author_id=row['author'],
-                                pub_date=row['pub_date'])
+                    review_id=row['review_id'],
+                    text=row['text'],
+                    author_id=row['author'],
+                    pub_date=row['pub_date'])
 
         self.stdout.write(self.style.SUCCESS('База данных успешно заполнена'))
